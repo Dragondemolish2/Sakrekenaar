043addd434a624cf81505ffddcb55e1ce1d7854e
Tjaart <tjaart008@gmail.com>
2025-07-23 21:58:48 +0200

    calculator

---
+++ app.py
@@ -0,0 +1,18 @@
+print("Calculator (type 'exit' to quit)")
+
+while True:  # while true creates a infinite loop so you can keep inputing diffierent calculations
+
+    # Get the users calculation example: (5 + 5)
+    calculation = input("Enter calculation: ")
+
+    # To end the calculator session
+    if calculation.lower() == 'exit':
+        print("Goodbye!")
+        break
+
+        # try except block to cacth errors were if the values are valid it print the result else give the error message
+    try:
+        result = eval(calculation)
+        print("Result:", result)
+    except Exception as e:
+        print("Invalid input. Please try again.")
